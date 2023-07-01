3f1e09ca593a3306d353a40d338803d5229f1bcd
Leon Luna <lyon.raices@gmail.com>
2023-07-01 14:28:45 -0300

    Freeze con extensiones del profe y otros cambios

--- Proyect/config/settings.py
+++ Proyect/config/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import sys
+APLIICACIONES = BASE_DIR / "apps"
+
+sys.path.append(str(APLIICACIONES))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
